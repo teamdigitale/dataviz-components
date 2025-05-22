3a72bb7acb5c9564e8d18ecf288bf60af048825b
Lorezz <point.point@gmail.com>
2025-05-22 10:28:43 +0200

    updated the example

--- example/src/SampleMap.tsx
+++ example/src/SampleMap.tsx
@@ -1,19 +1,18 @@
-import { RenderChart } from "dataviz-components";
-// import { FieldDataType, ClusterMapComponent } from  "dataviz-components";
+import { useState } from "react";
+import { RenderChart, ClusterMap, type PointData } from "dataviz-components";
+import { generateSamplePoints } from "./lib/generatePoints";
 
 function App() {
-  // const [pointsData] = useState<PointData[]>(() => generateSamplePoints(100));
+  const [pointsData] = useState<PointData[]>(() => generateSamplePoints(100));
   console.log("RenderChart", RenderChart);
   return (
-    <div>
-      {/* <ClusterMapComponent
-        pointsData={pointsData}
-        height={900}
-        clusterDistance={90}
-        initialCenter={[12.5, 42.5]} // Center on Italy
-        initialZoom={2}
-      /> */}
-    </div>
+    <ClusterMap
+      pointsData={pointsData}
+      height={500}
+      clusterDistance={90}
+      initialCenter={[12.5, 42.5]} // Center on Italy
+      initialZoom={2}
+    />
   );
 }
 
--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,16 +1,32 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import SampleBarchart from "./SampleBarchart";
 import SamplePiechart from "./SamplePiechart";
 import SampleLinechart from "./SampleLinechart";
 import SampleGeomapchart from "./SampleGeomapchart";
+import SampleMap from "./SampleMap";
 
 createRoot(document.getElementById("root")!).render(
-  <div style={{ padding: 30 }}>
-    <SampleBarchart />
-    <SamplePiechart />
-    <SampleLinechart />
-    <SampleGeomapchart />
+  <div style={{ padding: 30, width: 600 }}>
+    <div style={{ marginTop: 50 }}>
+      <h3>Bar Chart</h3>
+      <SampleBarchart />
+    </div>
+    <div style={{ marginTop: 50 }}>
+      <h3>Line Chart</h3>
+      <SampleLinechart />
+    </div>
+    <div style={{ marginTop: 50 }}>
+      <h3>Pie Chart</h3>
+      <SamplePiechart />
+    </div>
+    <div style={{ marginTop: 50, height: 700 }}>
+      <h3>GEO Map</h3>
+      <SampleGeomapchart />
+    </div>
+    <div style={{ marginTop: 50, marginBottom: 50, height: 600 }}>
+      <h3>Cluster Map</h3>
+      <SampleMap />
+    </div>
   </div>
 );
