3cbca6dc61423859485c7168b58cfffb9f9ef25d
Lorezz <point.point@gmail.com>
2025-05-22 10:05:33 +0200

    fix: export clustermap

---
+++ example/src/SampleMap.tsx
@@ -0,0 +1,20 @@
+import { RenderChart } from "dataviz-components";
+// import { FieldDataType, ClusterMapComponent } from  "dataviz-components";
+
+function App() {
+  // const [pointsData] = useState<PointData[]>(() => generateSamplePoints(100));
+  console.log("RenderChart", RenderChart);
+  return (
+    <div>
+      {/* <ClusterMapComponent
+        pointsData={pointsData}
+        height={900}
+        clusterDistance={90}
+        initialCenter={[12.5, 42.5]} // Center on Italy
+        initialZoom={2}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ChartWrapper from "./components/ChartWrapper";
 import RenderChart from "./components/RenderChart";
 import DataTable from "./components/DataTable";
+import ClusterMap from "./components/maps/ClusterMap";
 export * from "./types";
-export { ChartWrapper, RenderChart, DataTable };
+export { ChartWrapper, RenderChart, DataTable, ClusterMap };
